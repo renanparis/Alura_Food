050795e1ae6c24ae3710184628ca4931468474d0
renanparis <paris.oliveira@gmail.com>
2019-05-17 18:45:32 -0300

    Adição do layout do formulário.

---
+++ app/src/main/java/com/renanparis/alurafood/ui/activity/RegistrationFormActivity.java
@@ -0,0 +1,16 @@
+package com.renanparis.alurafood.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.renanparis.alurafood.R;
+
+public class RegistrationFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration_form_activity);
+    }
+}
