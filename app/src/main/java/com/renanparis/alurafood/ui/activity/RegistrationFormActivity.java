73a285f3a502afa4119cbfc73adf6ea4f1dc91c1
renanparis <paris.oliveira@gmail.com>
2019-05-20 21:58:51 -0300

    Validação do campo cpf, número de dígitos e cpf válido.

--- app/src/main/java/com/renanparis/alurafood/ui/activity/RegistrationFormActivity.java
+++ app/src/main/java/com/renanparis/alurafood/ui/activity/RegistrationFormActivity.java
@@ -3,11 +3,16 @@ package com.renanparis.alurafood.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 import com.google.android.material.textfield.TextInputLayout;
 import com.renanparis.alurafood.R;
+
+import br.com.caelum.stella.format.CPFFormatter;
+import br.com.caelum.stella.validation.CPFValidator;
+import br.com.caelum.stella.validation.InvalidStateException;
 
 public class RegistrationFormActivity extends AppCompatActivity {
 
@@ -48,8 +53,59 @@ public class RegistrationFormActivity extends AppCompatActivity {
     }
 
     private void configFieldCpf() {
-        TextInputLayout inputLayoutCpf = findViewById(R.id.registration_form_cpf);
-        checkFieldForm(inputLayoutCpf);
+        final TextInputLayout inputLayoutCpf = findViewById(R.id.registration_form_cpf);
+        final EditText fieldCpf = inputLayoutCpf.getEditText();
+        final CPFFormatter cpfFormatter = new CPFFormatter();
+
+        fieldCpf.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                String cpf = fieldCpf.getText().toString();
+                if (!hasFocus) {
+                    if (!confirmRequiredField(cpf, inputLayoutCpf)) return;
+                    if (!confirmRequiredElevenDigits(cpf, inputLayoutCpf)) return;
+                    if (!confirmValidCPF(cpf, inputLayoutCpf)) return;
+                    removeError(inputLayoutCpf);
+
+                    String cpfFormat = cpfFormatter.format(cpf);
+                    fieldCpf.setText(cpfFormat);
+
+                }else {
+                    try {
+                        String cpfUnformatted = cpfFormatter.unformat(cpf);
+                        fieldCpf.setText(cpfUnformatted);
+                    }catch (IllegalArgumentException e){
+                        Log.e("erro formatação cpf", e.getMessage());
+                    }
+
+                }
+            }
+        });
+
+    }
+
+    private boolean confirmValidCPF(String cpf, TextInputLayout inputLayoutCpf) {
+        try {
+            CPFValidator cpfValidator = new CPFValidator();
+            cpfValidator.assertValid(cpf);
+        } catch (InvalidStateException e) {
+            inputLayoutCpf.setError("CPF inválido");
+            return false;
+        }
+        return true;
+    }
+
+    private void removeError(TextInputLayout inputLayout) {
+        inputLayout.setError(null);
+        inputLayout.setErrorEnabled(false);
+    }
+
+    private boolean confirmRequiredElevenDigits(String cpf, TextInputLayout inputLayoutCpf) {
+        if (cpf.length() != 11) {
+            inputLayoutCpf.setError("CPF precisa ter 11 dígitos");
+            return false;
+        }
+        return true;
     }
 
     private void configFieldName() {
@@ -63,20 +119,21 @@ public class RegistrationFormActivity extends AppCompatActivity {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 String text = field.getText().toString();
-                if (!hasFocus){
-                    confirmRequiredField(text, inputLayoutField);
+                if (!hasFocus) {
+                    if (!confirmRequiredField(text, inputLayoutField)) return;
+                    removeError(inputLayoutField);
                 }
             }
         });
     }
 
-    private void confirmRequiredField(String text, TextInputLayout inputLayoutField) {
+    private boolean confirmRequiredField(String text, TextInputLayout inputLayoutField) {
         if (text.isEmpty()) {
             inputLayoutField.setError("Campo Obrigatório");
-        }else{
-            inputLayoutField.setError(null);
-            inputLayoutField.setErrorEnabled(false);
+            return false;
         }
+        return true;
+
     }
 }
 
